984731b042c834e8f955b21c56735ddb2a42da4a
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-08-29 09:08:43 -0700

    cleaned up error handling code with error class

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const HttpError = require('../models/http-error');
+
 const router = express.Router();
 
 const DUMMY_PLACES = [
@@ -23,9 +25,7 @@ router.get('/:pid', (req, res, next) => {
   });
 
   if (!place) {
-    const error = new Error('Could not find a place for the specified place id.');
-    error.code = 404;
-    throw error; // okay for non-asyncronous code, use next for asyncronous code
+    throw new HttpError('Could not find a place for the specified place id.', 404);
   }
 
   res.json({ place });
@@ -38,9 +38,7 @@ router.get('/user/:uid', (req, res, next) => {
   });
 
   if (!place) {
-    const error = new Error('Could not find a place for the specified user id.');
-    error.code = 404;
-    return next(error); // forward to the next error handling middleware in line
+    return next(new HttpError('Could not find a place for the specified user id.', 404));
   }
 
   res.json({ place });
