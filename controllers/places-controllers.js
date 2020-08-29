6ca8bff07a5bc6745ce99316321fa425bfaebe3b
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-08-29 09:48:00 -0700

    set up post route functionality

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,4 +1,5 @@
 const HttpError = require('../models/http-error');
+const { v4: uuid } = require('uuid')
 
 const DUMMY_PLACES = [
   {
@@ -40,5 +41,23 @@ const getPlaceByUserId = (req, res, next) => {
   res.json({ place });
 }
 
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+  const createdPlace = {
+    id: uuid(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator
+  };
+
+  DUMMY_PLACES.push(createPlace);
+
+  res.status(201).json({ place: createdPlace })
+
+}
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
