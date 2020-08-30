c51708cf28eae0253caf7dacf9b01c09cdd4410d
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-08-30 07:45:29 -0700

    finish routes for user-routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const placesRoutes = require('./routes/places-routes');
+const usersRoutes = require('./routes/users-routes');
 const HttpError = require('./models/http-error')
 
 const app = express();
@@ -9,6 +10,7 @@ const app = express();
 app.use(bodyParser.json())
 
 app.use('/api/places', placesRoutes); // filter only placesRoutes that start with /api/places
+app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => { // will only be run if none of the placeRoutes send a response. 
   const error = new HttpError('Could not find this route.', 404);
