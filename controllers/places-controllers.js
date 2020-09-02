cdd3fd639950c4d1c3fbac357c9b4adc209fb633
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-09-02 10:37:24 -0700

    implemented delete functionality

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -163,16 +163,26 @@ const deletePlace = async (req, res, next) => {
   const placeId = req.params.pid;
   let place;
   try {
-    place = await Place.findById(placeId);
+    place = await Place.findById(placeId).populate('creator');
   } catch (err) {
-    const error = new HttpError('Something went wrong, could not delete place', 500)
+    const error = new HttpError('Something went wrong, could not delete place', 500);
     return next(error)
   }
 
+  if (!place) {
+    const error = new HttpError('Could not find place for this id', 404);
+    return next(error);
+  }
+
   try {
-    await place.remove();
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    await place.remove({ session: sess });
+    place.creator.places.pull(place);
+    await place.creator.save({ session: sess });
+    await sess.commitTransaction();
   } catch (err) {
-    const error = new HttpError('Something went wrong, could not delete place', 500)
+    const error = new HttpError('Something went wrong, could not delete place', 500);
     return next(error)
   }
 
