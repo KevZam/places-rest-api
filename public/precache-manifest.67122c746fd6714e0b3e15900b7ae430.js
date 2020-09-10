cb9ac4fbe86fad481f80c41976fe4d3c8e64d59d
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-09-10 14:07:01 -0700

    added new react app front end version

---
+++ public/precache-manifest.67122c746fd6714e0b3e15900b7ae430.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7a50ae1fa8f59e2748ec58fb812c769",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ed821b178f634670854",
+    "url": "/static/css/0.ce71ab98.chunk.css"
+  },
+  {
+    "revision": "4498dff61c88937d9772",
+    "url": "/static/css/4.cfa93c1f.chunk.css"
+  },
+  {
+    "revision": "41bb03b22d3b05811305",
+    "url": "/static/css/5.07ee2198.chunk.css"
+  },
+  {
+    "revision": "974ac2fa5def912cdc60",
+    "url": "/static/css/6.59e4fcd5.chunk.css"
+  },
+  {
+    "revision": "a396f393def8bbc6e936",
+    "url": "/static/css/7.9ad3aa59.chunk.css"
+  },
+  {
+    "revision": "4c79afdcd7936d7937fb",
+    "url": "/static/css/8.528aec34.chunk.css"
+  },
+  {
+    "revision": "c48cd4f03e2249773af7",
+    "url": "/static/css/main.a30cd142.chunk.css"
+  },
+  {
+    "revision": "7ed821b178f634670854",
+    "url": "/static/js/0.9e360f0c.chunk.js"
+  },
+  {
+    "revision": "54f6f11619eff4a3584a",
+    "url": "/static/js/3.0c4894d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/3.0c4894d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4498dff61c88937d9772",
+    "url": "/static/js/4.8ccaa7c2.chunk.js"
+  },
+  {
+    "revision": "41bb03b22d3b05811305",
+    "url": "/static/js/5.0635a26c.chunk.js"
+  },
+  {
+    "revision": "974ac2fa5def912cdc60",
+    "url": "/static/js/6.2161eb0a.chunk.js"
+  },
+  {
+    "revision": "a396f393def8bbc6e936",
+    "url": "/static/js/7.61c8d1f8.chunk.js"
+  },
+  {
+    "revision": "4c79afdcd7936d7937fb",
+    "url": "/static/js/8.66a74443.chunk.js"
+  },
+  {
+    "revision": "c48cd4f03e2249773af7",
+    "url": "/static/js/main.aa87ad86.chunk.js"
+  },
+  {
+    "revision": "664cdf7ecd651548085f",
+    "url": "/static/js/runtime-main.60cb1882.js"
+  }
+]);
